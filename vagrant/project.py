b1854e673458547e719d7a4782dbd6febf8560b5
Jessica Langston <jessica.langston11@gmail.com>
2017-08-28 16:31:08 -0700

    User can view restaurants and menus, edit and delete menu items

---
+++ vagrant/project.py
@@ -0,0 +1,77 @@
+from flask import Flask, redirect, render_template, request, url_for
+app = Flask(__name__)
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
+
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+# 'routing functions' or 'decorators' are stackable. this is useful for having
+# multiple routes go to the same page
+@app.route('/')
+def index():
+    restaurants = session.query(Restaurant).all()
+    return render_template('index.html', restaurants=restaurants)
+
+
+@app.route('/restaurant/<int:restaurant_id>/')
+def restaurantMenu(restaurant_id):
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+    items = session.query(MenuItem).filter_by(restaurant_id=restaurant.id)
+    return render_template('menu.html', restaurant=restaurant, items=items)
+
+
+@app.route('/restaurant/<int:restaurant_id>/new/', methods=['GET', 'POST'])
+def newMenuItem(restaurant_id):
+    if request.method == 'POST':
+        newItem = MenuItem(name=request.form['name'], restaurant_id=restaurant_id)
+        session.add(newItem)
+        session.commit()
+        return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
+    else:
+        return render_template('newmenuitem.html', restaurant_id=restaurant_id)
+
+
+@app.route('/restaurant/<int:restaurant_id>/<int:menu_id>/edit', methods=['GET', 'POST'])
+def editMenuItem(restaurant_id, menu_id):
+    editedItem = session.query(MenuItem).filter_by(id=menu_id).one()
+    if request.method == 'POST':
+        if request.form['name']:
+            editedItem.name = request.form['name']
+        session.add(editedItem)
+        session.commit()
+        return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
+    else:
+        return render_template('editmenuitem.html', restaurant_id=restaurant_id, menu_id=menu_id, item=editedItem)
+
+
+@app.route('/restaurant/<int:restaurant_id>/<int:menu_id>/delete', methods=['GET', 'POST'])
+def deleteMenuItem(restaurant_id, menu_id):
+    item = session.query(MenuItem).filter_by(id=menu_id).one()
+    if request.method == "POST":
+        session.delete(item)
+        session.commit()
+        return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
+    else: 
+        return render_template('deletemenuitem.html', item=item)
+
+
+if __name__ == '__main__':
+    
+    # with app.debug = True
+    # server will reload when code changes, so you don't have to restart it
+    # also provides debugger tool in the browser
+    app.debug = True 
+
+    # since we're using a vagrant environment, we must make the server publicly
+    # available. host = '0.0.0.0' tells it to listen on all public IP addresses
+    app.run(host = '0.0.0.0', port = 5000)
+
+
+# benefits of using a framework:
+# we don't have to write out response codes, like we did in webserver.py
